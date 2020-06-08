810845e956de6e9909c94458de74f77fda75fa1d
FlorianGERMAIN <florian.germain@free.fr>
2020-06-08 14:24:27 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tqt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'TechnicalQuizToolFrontEnd';
+}
