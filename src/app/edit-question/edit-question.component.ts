90aa8c2f27ad431349815bd513f1990bc8267385
FlorianGERMAIN <florian.germain@free.fr>
2020-06-09 15:31:37 +0200

    editing edit-question component

--- src/app/edit-question/edit-question.component.ts
+++ src/app/edit-question/edit-question.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionService } from 'src/services/question.service';
+import { Question } from '../models/question';
 
 @Component({
   selector: 'tqt-edit-question',
@@ -8,13 +9,26 @@ import { QuestionService } from 'src/services/question.service';
 })
 export class EditQuestionComponent implements OnInit {
   questions : any=[];
+  question : Question = new Question(4,"Angular","easy","Ceci est la 4eme question",["ah?","non","peut-etre","la réponse D"]);
 
   constructor(private questionService:QuestionService) { }
 
   ngOnInit(): void {
     this.questions = this.questionService.get();
+    this.SelectQuestion();
+  }
+
+  SelectQuestion(){
+    this.question = this.questions[2];
   }
 
 
+  onSaveQuestion(obj){
+    console.log("objet question :");
+    console.log(this.question);
+   
+   
+
+  }
 
 }
