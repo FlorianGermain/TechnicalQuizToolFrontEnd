fd0990155fc7de5def53e2bb1a71110405bbae3c
FlorianGERMAIN <florian.germain@free.fr>
2020-06-09 14:21:58 +0200

    editing edit-question component

--- src/services/question.service.ts
+++ src/services/question.service.ts
@@ -15,9 +15,10 @@ export class QuestionService {
       // new Question(2,"Angular","easy","Ceci est la 2eme question","vrai","non","surement","la réponse D")
        
       new Question(1,"Angular","easy","Ceci est la premiere question",["vrai","non","je ne sais pas","la réponse D"]),
-      new Question(2,"Angular","easy","Ceci est la 2eme question",["vrai","non","surement","la réponse D"]),
-      new Question(2,"Angular","easy","Ceci est la 3eme question",["ah bon?","non","peut-etre","la réponse D"])
-     
+      //new Question(1,"Angular","easy","Ceci est la premiere question",[["vrai"],["non"],["je ne sais pas"],["la réponse D"]]),
+      new Question(2,"CSharp","hard","Ceci est la 2eme question",["vrai","non","surement","la réponse D"]),
+      new Question(3,"SQL","medium","Ceci est la 3eme question",["ah bon?","non","peut-etre","la réponse D"]),
+      new Question(4,"Angular","easy","Ceci est la 4eme question",["ah?","non","peut-etre","la réponse D"])
     ];
 
   }
